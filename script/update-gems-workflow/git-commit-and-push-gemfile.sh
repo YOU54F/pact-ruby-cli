30f3b8c6b9a42137a99b8e43b3fe142ceba43c29
Yousaf Nabi <you@saf.dev>
2023-05-19 17:33:52 +0100

    chore: ensure when a gem is updated, we use that version to update the gem semver

--- script/update-gems-workflow/git-commit-and-push-gemfile.sh
+++ script/update-gems-workflow/git-commit-and-push-gemfile.sh
@@ -3,12 +3,7 @@
 set -euo >/dev/null
 
 git add Gemfile.lock
-PACT_CLI_DOCKER_VERSION=$(cat VERSION)
-rm VERSION
-bundle exec bump ${RELEASED_GEM_INCREMENT:-minor} --no-commit
-PACT_CLI_VERSION=$(ruby -I lib -e "require 'pact/cli/version.rb'; puts Pact::Cli::VERSION")
 git add lib/pact/cli/version.rb 
-echo $PACT_CLI_VERSION > VERSION
 
 if [ -n "${RELEASED_GEM_NAME}" ] && [ -n "${RELEASED_GEM_VERSION}" ]; then
   git commit -m "feat(gems): pact-cli v$PACT_CLI_VERSION: update ${RELEASED_GEM_NAME} gem to version ${RELEASED_GEM_VERSION}
--- script/update-gems-workflow/update-gems.sh
+++ script/update-gems-workflow/update-gems.sh
@@ -2,9 +2,21 @@
 
 set -euo >/dev/null
 
+## Update the version of the CLI in the Gemfile.lock and lib/pact/cli/version.rb
+## bundle exec bump updates VERSION instead of pact/cli/version.rb
+## so we store it, update our bundle, and then write it back
+## the PACT_CLI_DOCKER_VERSION is our Docker semantic tag
+PACT_CLI_DOCKER_VERSION=$(cat VERSION)
+rm VERSION
+bundle exec bump ${RELEASED_GEM_INCREMENT:-minor} --no-commit
+PACT_CLI_VERSION=$(ruby -I lib -e "require 'pact/cli/version.rb'; puts Pact::Cli::VERSION")
+
 if [ -n "${RELEASED_GEM_NAME:-}" ] && [ -n "${RELEASED_GEM_VERSION:-}" ]; then
   gem install ${RELEASED_GEM_NAME} -v ${RELEASED_GEM_VERSION}
   bundle update ${RELEASED_GEM_NAME}
 else
   bundle update
 fi
+
+## Make sure we write our original PACT_CLI_DOCKER_VERSION back
+echo $PACT_CLI_VERSION > VERSION
