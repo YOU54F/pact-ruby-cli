135e6794c4400115e16838bb3b28043d79bd7c53
Yousaf Nabi <you@saf.dev>
2023-05-22 19:16:30 +0100

    chore(ci): audit - only build indiv images per audit

--- script/release-workflow/set-env-vars.sh
+++ script/release-workflow/set-env-vars.sh
@@ -2,7 +2,16 @@
 
 set -e
 
-export DOCKER_IMAGE_ORG_AND_NAME=pactfoundation/pact-cli
+export DOCKER_IMAGE_ORG_AND_NAME=${DOCKER_IMAGE_ORG_AND_NAME:-'pactfoundation/pact-cli'}
+if [ $DOCKER_TARGET_PLATFORM ]; then
+    export ARCH=$(echo "$DOCKER_TARGET_PLATFORM" | sed 's/linux\///' | sed 's/\/v.*//')
+    export ARCHES="$ARCH"
+    export $ARCHES
+else 
+    ARCHES='"amd64" "arm64" "arm"'
+    export $ARCHES
+    export ARCH=${ARCHES[0]}
+fi
 
 if [ -n "${CUSTOM_TAG:-}" ]; then
   export TAG=$CUSTOM_TAG
