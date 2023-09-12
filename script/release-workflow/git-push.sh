9941e61d3f13ab75891b06a25a4a293553c062a7
Yousaf Nabi <yousaf@pactflow.io>
2023-09-12 15:49:17 +0100

    chore(ci): trial updating pact-cli version based on Docker changes

--- script/release-workflow/git-push.sh
+++ script/release-workflow/git-push.sh
@@ -2,6 +2,9 @@
 
 set -euo >/dev/null
 
+git add Gemfile.lock
+git add VERSION lib/pact/cli/version.rb
+git commit -m "chore(release): version ${TAG}"
 git tag -a "${TAG}" -m "chore(release): version ${TAG}"
 git push origin ${TAG}
 git push origin master
