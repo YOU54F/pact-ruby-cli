fd8a85fad6559a8788cfae13e6f312bc7b4cfe5f
Yousaf Nabi <yousaf@pactflow.io>
2023-09-12 16:18:34 +0100

    chore(ci): only add/commit back if changes

--- script/release-workflow/git-push.sh
+++ script/release-workflow/git-push.sh
@@ -2,9 +2,10 @@
 
 set -euo >/dev/null
 
-git add Gemfile.lock
-git add VERSION lib/pact/cli/version.rb
-git commit -m "chore(release): version ${TAG}"
+if [[ $(git status --porcelain) ]]; then
+    git add Gemfile.lock VERSION lib/pact/cli/version.rb
+    git commit -m "chore(release): version ${TAG}"
+fi
 git tag -a "${TAG}" -m "chore(release): version ${TAG}"
 git push origin ${TAG}
 git push origin master
